37f5131d2ab405beb721c79519c9224ac60471d2
nathaliamkp <nah.manso@gmail.com>
2024-01-29 15:10:08 -0300

    creatingCrudMethodsInServices

--- src/main/java/com/test/nathalia/service/ClientService.java
+++ src/main/java/com/test/nathalia/service/ClientService.java
@@ -1,9 +1,18 @@
 package com.test.nathalia.service;
 
+import com.test.nathalia.entity.Client;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface ClientService {
+
+    void saveClient();
+
+    Client getClientById();
+
+    void delelteClient();
 
 
 
--- src/main/java/com/test/nathalia/service/EnterpriseService.java
+++ src/main/java/com/test/nathalia/service/EnterpriseService.java
@@ -1,9 +1,16 @@
 package com.test.nathalia.service;
 
+import com.test.nathalia.entity.Client;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EnterpriseService {
 
+
+    void saveEnterprise();
+
+    Client getEnterprise();
+
+    void delelteEnterprise();
 
 }
