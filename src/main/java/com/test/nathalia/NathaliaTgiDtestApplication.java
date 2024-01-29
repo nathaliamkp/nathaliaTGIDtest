34e86178d71f43195f8e13ede8d55cb792d6230a
nathaliamkp <nah.manso@gmail.com>
2024-01-29 10:38:31 -0300

    first commit

---
+++ src/main/java/com/test/nathalia/NathaliaTgiDtestApplication.java
@@ -0,0 +1,13 @@
+package com.test.nathalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NathaliaTgiDtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NathaliaTgiDtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/nathalia/NathaliaTgiDtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.nathalia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NathaliaTgiDtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
